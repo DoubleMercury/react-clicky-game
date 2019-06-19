961d033a48817bf9837f7f7065537bb9e098a1dc
Double Mercury <maafakeout@yahoo.com>
2019-06-19 13:51:41 -0400

    Updates

---
+++ precache-manifest.5464f94c289259c5a44bdf2b3ffa0afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d6c25ea28fb5bac48154590c61210b",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "5e19560e8b7ffcb4af61",
+    "url": "/react-clicky-game/static/css/main.3cf53f6c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5e19560e8b7ffcb4af61",
+    "url": "/react-clicky-game/static/js/main.a6c32330.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
